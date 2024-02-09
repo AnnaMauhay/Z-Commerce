4d0563f83516a54a2ddce0bf22a742ef0d59cd56
Anna Liza Mauhay <mauhay.anna.liza@gmail.com>
2024-02-09 15:47:01 +0100

    ZEC-78 Implement API key filter

--- src/main/java/com/zalando/ecommerce/filter/ApiKeyAuthFilter.java
+++ src/main/java/com/zalando/ecommerce/filter/ApiKeyAuthFilter.java
@@ -1,5 +1,6 @@
 package com.zalando.ecommerce.filter;
 
+import com.nimbusds.jose.shaded.gson.JsonObject;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,23 +11,51 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 public class ApiKeyAuthFilter extends OncePerRequestFilter {
     @Value("${api.key}")
     private String apiKey;
-    @Value("${api.secret}")
-    private String apiSecret;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String requestApiKey = request.getHeader("X-API-KEY");
-        String requestApiSecret = request.getHeader("X-API-SECRET");
-        if (apiKey.equals(requestApiKey) && apiSecret.equals(requestApiSecret)) {
+        if (apiKey.equals(requestApiKey)) {
             filterChain.doFilter(request, response);
         } else {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            response.getWriter().write("Unauthorized");
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            JsonObject json = new JsonObject();
+            json.addProperty("createdAt", LocalDateTime.now().toString());
+            json.addProperty("statusCode", HttpStatus.UNAUTHORIZED.value());
+            json.addProperty("body", "Unauthorized. API Key was invalid or not provided.");
+
+            response.getWriter().write(json.toString());
         }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        List<String> pathList = List.of(
+                "/error",
+                "/api-docs",
+                "/swagger-resources",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui",
+                "/h2-console");
+
+        for (String item : pathList) {
+            if (path.contains(item)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
